91f81d3ec19d255e28ae82b81aa7bbcb80688e1e
Jessica <jessicacotrina@gmail.com>
2020-12-11 17:38:13 -0500

    adding budgets update

--- server/api/budgets.js
+++ server/api/budgets.js
@@ -1,6 +1,6 @@
 const router = require('express').Router()
-const {Budget, Category, User} = require('../db/models')
-
+const {Budget} = require('../db/models')
+const {Category} = require('../db/models')
 module.exports = router
 
 router.get('/:userId', async (req, res, next) => {
@@ -9,53 +9,28 @@ router.get('/:userId', async (req, res, next) => {
       include: Category,
       where: {
         userId: req.params.userId,
+        period: req.query.period,
       },
     })
+
     res.json(budgets)
   } catch (err) {
     next(err)
   }
 })
 
-//GET /api/budgets
-router.get("/", async (req, res, next) => {
+router.put('/:budgetId', async (req, res, next) => {
   try {
-     const allBudgets = await Budget.findAll();
-     res.send(allBudgets) 
+    const updatedBudget = await Budget.findByPk(req.params.budgetId)
+    const data = {
+      total: req.body.amount,
+    }
+    await updatedBudget.update(data)
+    res.send(updatedBudget)
   } catch (error) {
     next(error)
   }
 })
-
-//GET /api/budgets/:budgetId
-router.get('/:budgetId', async (req, res, next) => {
-  try {
-    const budget = await Budget.findByPk(req.params.budgetId, {
-      include: Category,
-      include: User,
-    })
-    res.send(budget)
-  } catch (error) {
-    next(error)
-  }
-})
-
-//POST /api/budgets
-router.post('/', async (req, res, next) => {
-  try {
-    const { total, userId, categoryId } = req.body
-    console.log('req.body', req.body)
-    const newBudget = await Budget.create({
-      total,
-      userId,
-      categoryId,
-    })
-    res.send(newBudget)
-  } catch (error) {
-    next(error)
-  }
-})
-
 
 // DELETE: budget/:budgetId
 router.delete('/:budgetId', async (req, res, next) => {
